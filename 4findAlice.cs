0f072aa253b80917c5dd4123dcd2d5867df13bd0
Jessica Nations <jessica.nations@gmail.com>
2018-05-04 07:11:52 -0500

    more

--- 4findAlice.cs
+++ 4findAlice.cs
@@ -21,3 +21,41 @@ public static string getBetween(string strSource, string strStart, string strEnd
         return "";
     }
 }
+
+/*https://stackoverflow.com/questions/10709821/find-text-in-string-with-c-sharp
+OR
+
+static void Main(string[] args)
+    {
+
+        int f = 0;
+        Console.WriteLine("enter the string");
+        string s = Console.ReadLine();
+        Console.WriteLine("enter the word to be searched");
+        string a = Console.ReadLine();
+        int l = s.Length;
+        int c = a.Length;
+
+        for (int i = 0; i < l; i++)
+        {
+            if (s[i] == a[0])
+            {
+                for (int K = i + 1, j = 1; j < c; j++, K++)
+                {
+                    if (s[K] == a[j])
+                    {
+                        f++;
+                    }
+                }
+            }
+        }
+        if (f == c - 1)
+        {
+            Console.WriteLine("matching");
+        }
+        else
+        {
+            Console.WriteLine("not found");
+        }
+        Console.ReadLine();
+    } */
