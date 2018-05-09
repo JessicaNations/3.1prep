94a4fa82caf03f1721a88dbe2ec75e1e96f7e728
Jessica Nations <jessica.nations@gmail.com>
2018-05-09 15:23:09 +0000

    Update

--- 1STUDIOCircleArea.cs
+++ 1STUDIOCircleArea.cs
@@ -7,13 +7,13 @@ namespace CirleArea
     {
         public static void Main(string[] args)
         {
-            int radius;
+            double radius;
             double area;
             string input;
 
             Console.WriteLine("Enter a radius:");
             input = Console.ReadLine();
-            radius = Convert.ToInt32.(Console.ReadLine());
+            radius = double Convert.parse(input);
 
             area = Math.PI * radius * radius;
             Console.WriteLine($"The area of a circle with radius {radius} is: {area}");
